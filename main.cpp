9e100ca84660181c086c072e188da820eeabc4ff
u211-11 <u211-11@public.mpei.local>
2017-05-11 15:21:31 +0300

    vtoroi commit

--- main.cpp
+++ main.cpp
@@ -13,11 +13,27 @@ void print_byte (uint8_t byte){
     cout<<nible_to_hex(high)<<nible_to_hex(low);
 }
 
+void  print_in_hex (const void* data, size_t size){
+    const char* p= reinterpret_cast<const char*>(data);
+    for (size_t i=0; i<size; i++){
+        print_byte(p[i]);
+        cout<<" ";
+    }}
+
+
+
 int
 main() {
     uint32_t byte;
+    int N;
+    size_t size;
         cout<<"vvedite byte\n";
        cin>>byte;
+
     print_byte(byte);
+
+    cout<<"\nN = ";
+    cin>>N;
+     print_in_hex (&N,sizeof(N));
     return 0;
 }
